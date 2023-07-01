61d5ab27564400185b0cf07716a22496150d1215
Jo X <39098450+Tamijo-Netrunner@users.noreply.github.com>
2023-07-01 09:51:00 +0100

    1

--- Clients/nrdb_client.py
+++ Clients/nrdb_client.py
@@ -19,35 +19,35 @@ class NrdbClient:
     Authentication: None, for public APIs.  This app does not use private APIs.
     """
     
-        """
-        Card 01001 is defined as:
-        {"imageUrlTemplate":"https://static.nrdbassets.com/v1/large/{code}.jpg",
-        "data":
-            [{"base_link":0,
-            "code":"01001",
-            "deck_limit":1,
-            "faction_code":"anarch",
-            "faction_cost":0,
-            "flavor":"\"Watch this. It'll be funny.\"",
-            "illustrator":"Ralph Beisner",
-            "influence_limit":15,
-            "keywords":"G-mod",
-            "minimum_deck_size":45,
-            "pack_code":"core",
-            "position":1,
-            "quantity":1,
-            "side_code":"runner",
-            "stripped_text":"Whenever you install a virus program, the Corp trashes the top card of R&D.",
-            "stripped_title":"Noise: Hacker Extraordinaire",
-            "text":"Whenever you install a <strong>virus</strong> program, the Corp trashes the top card of R&D.",
-            "title":"Noise: Hacker Extraordinaire",
-            "type_code":"identity",
-            "uniqueness":false}],
-        "total":1,
-        "success":true,
-        "version_number":"2.0",
-        "last_updated":"2021-03-13T05:08:22+00:00"}
-        """
+    """
+    Card 01001 is defined as:
+    {"imageUrlTemplate":"https://static.nrdbassets.com/v1/large/{code}.jpg",
+    "data":
+        [{"base_link":0,
+        "code":"01001",
+        "deck_limit":1,
+        "faction_code":"anarch",
+        "faction_cost":0,
+        "flavor":"\"Watch this. It'll be funny.\"",
+        "illustrator":"Ralph Beisner",
+        "influence_limit":15,
+        "keywords":"G-mod",
+        "minimum_deck_size":45,
+        "pack_code":"core",
+        "position":1,
+        "quantity":1,
+        "side_code":"runner",
+        "stripped_text":"Whenever you install a virus program, the Corp trashes the top card of R&D.",
+        "stripped_title":"Noise: Hacker Extraordinaire",
+        "text":"Whenever you install a <strong>virus</strong> program, the Corp trashes the top card of R&D.",
+        "title":"Noise: Hacker Extraordinaire",
+        "type_code":"identity",
+        "uniqueness":false}],
+    "total":1,
+    "success":true,
+    "version_number":"2.0",
+    "last_updated":"2021-03-13T05:08:22+00:00"}
+    """
         
     def get_card(self, code)
         return https://netrunnerdb.com/api/2.0/public/card/01001
