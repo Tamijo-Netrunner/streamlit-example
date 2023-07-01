0d600ab1e96f650b9c71d5f41c590e3342b6e088
Jo X <39098450+Tamijo-Netrunner@users.noreply.github.com>
2023-07-01 09:55:58 +0100

    1

---
+++ data/mappings.py
@@ -0,0 +1,33 @@
+BASE_FACTION_GLYPH_PATH = "assets/NSG-Visual-Assets/SVG/Faction Glyphs/{}"
+FACTION_ASSETS = {
+    "anarch": {
+        "glyph_path": "NISEI_ANARCH.svg"
+    },
+    "criminal": {
+        "glyph_path": "NISEI_CRIMINAL.svg"
+    },
+    "shaper": {
+        "glyph_path": "NISEI_SHAPER.svg"
+    },
+    "haas-bioroid": {
+        "glyph_path": "NISEI_HB.svg"
+    },
+    "jinteki": {
+        "glyph_path": "NISEI_JINTEKI.svg"
+    },
+    "nbn": {
+        "glyph_path": "NISEI_NBN.svg"
+    },
+    "weyland-consortium": {
+        "glyph_path": "NISEI_WEYLAND.svg"
+    },
+    "apex": {
+        "glyph_path": "NISEI_APEX.svg"
+    },
+    "adam": {
+        "glyph_path": "NISEI_ADAM.svg"
+    },
+    "sunny-lebeau": {
+        "glyph_path": "NISEI_SUNNY.svg"
+    }
+}
